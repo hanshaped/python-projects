fae1a1787aeaba8a1ebab1ede1ca9e5229a96207
Hanna Szczepanek <61844552+hanshaped@users.noreply.github.com>
2020-03-31 18:47:30 +0200

    Implemented fetching the whole document

--- webcrawler.py
+++ webcrawler.py
@@ -5,3 +5,14 @@ Created on Tue Mar 31 18:37:56 2020
 @author: Hanna Szczepanek
 """
 
+import urllib.request
+from bs4 import BeautifulSoup
+
+print('Getting staff urls...')
+
+staff_url = 'http://wa.amu.edu.pl/wa/en/staff_list'
+response = urllib.request.urlopen(staff_url)
+data = response.read()
+doc = BeautifulSoup(data, 'html.parser')
+
+print(doc)
