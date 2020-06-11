a34f0367e59152533239b253498d222ca7fb0bf2
Hanna Szczepanek <61844552+hanshaped@users.noreply.github.com>
2020-06-11 16:57:43 +0200

    importing library and defining function

--- translate.py
+++ translate.py
@@ -5,3 +5,11 @@ Created on Thu Jun 11 16:43:18 2020
 @author: Hanna Szczepanek
 """
 
+from googletrans import Translator
+
+def main():
+    translator = Translator()
+    result = translator.translate('How do you do?', dest='pl')
+    print(result)
+    
+main()
